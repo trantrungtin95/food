bb4100b866afa07ccee36f4998b10f09e8bb10a7
tin tran <tintran@Macbooks-MacBook-Pro.local>
2020-09-04 20:38:31 +0700

    Setup view store

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
     belongs_to :user
-    has_many :dishes
+    has_many :dishes, :dependent => :destroy
     scope :find_restaurants_name, -> (search){ where("name LIKE ?", "%#{search}%")}
 end
