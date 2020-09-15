9ed5cbeba45d679dc27da63a24115824684694c1
tin tran <tintran@Macbooks-MacBook-Pro.local>
2020-09-15 20:57:56 +0700

    Update Layout part 1

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,7 @@
 class StoreController < ApplicationController
   skip_before_action :authorize
+  layout 'store_application'
+
   def index
     @dishes = Dish.paginate(:page => params[:page], :per_page => 10).order('created_at desc')
     @cart = current_cart
