8264c85edacdf5292ca7b96b0c48745a6cb8f484
tin tran <tintran@Macbooks-MacBook-Pro.local>
2020-09-20 16:08:52 +0700

    Specify the path

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("jquery.star-rating-svg.min")
 
-import('styles/application.css');
+import('../styles/application.css');
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
