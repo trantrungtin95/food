e36e835a31c572a1507adbc298adc73992d88d69
tin tran <tintran@Macbooks-MacBook-Pro.local>
2020-10-23 19:31:55 +0700

    Config 6

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,17 +109,13 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_mailer.delivery_method = :smtp
-  host = 'example.com' #replace with your own url
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = {host: "localhost:3000"}
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.active_job.queue_adapter = :inline
 
-  # SMTP settings for gmail
-  config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :user_name            => <trantrungtinbkit@gmail.com>,
-    :password             => <Trungtin001>,
-    :authentication       => "plain",
-    :enable_starttls_auto => true
+  config.action_mailer.default_options  = {
+    from:  "trantrungtinbkit@gmail.com"
   }
 end
