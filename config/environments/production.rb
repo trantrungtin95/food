e22ad66c17b0322bec9b6c1021415259486060bf
tin tran <tintran@Macbooks-MacBook-Pro.local>
2020-10-25 14:01:14 +0700

    Config 7

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,6 +109,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.default_url_options = {host: "localhost:3000"}
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :letter_opener
