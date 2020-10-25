0bccc48c966153388a9048cbb582231976188056
tin tran <tintran@Macbooks-MacBook-Pro.local>
2020-10-25 14:09:35 +0700

    Use gmail as email service on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,14 +109,19 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.action_mailer.default_url_options = {host: "localhost:3000"}
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.active_job.queue_adapter = :inline
+  
 
-  config.action_mailer.default_options  = {
-    from:  "trantrungtinbkit@gmail.com"
+  config.action_mailer.delivery_method = :smtp
+  host = 'example.com' #replace with your own url
+  config.action_mailer.default_url_options = { host: host }
+
+# SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => ENV['GMAIL_USERNAME'],
+    :password             => ENV['GMAIL_PASSWORD'],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
   }
 end
