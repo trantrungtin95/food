7d656e77049531ee1107f4b25133160fd3c531f1
tin tran <tintran@Macbooks-MacBook-Pro.local>
2020-12-19 20:24:59 +0700

    Commit 13

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,9 +119,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :domain               => 'gmail.com',
-    :user_name            => 'trantrungtinbkit@gmail.com',
-    :password             => 'Trungtin001',
+    :user_name            => ENV['GMAIL_USERNAME'],
+    :password             => ENV['GMAIL_PASSWORD'],
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
