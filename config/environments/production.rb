4d550def33266884e66daf137acfc557ecae9f4b
tin tran <tintran@Macbooks-MacBook-Pro.local>
2020-12-19 10:32:13 +0700

    Commit 9

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,7 +112,7 @@ Rails.application.configure do
   
 
   config.action_mailer.delivery_method = :smtp
-  host = 'example.com' #replace with your own url
+  host = 'restaurants-online.herokuapp.com' #replace with your own url
   config.action_mailer.default_url_options = { host: host }
 
 # SMTP settings for gmail
