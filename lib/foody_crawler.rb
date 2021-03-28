474132c9ec2f01e2e09f58a88abcdfc034cf60fc
tin tran <tintran@Macbooks-MacBook-Pro.local>
2021-03-28 08:24:19 +0700

    Commit 22

--- lib/foody_crawler.rb
+++ lib/foody_crawler.rb
@@ -2,7 +2,7 @@ class FoodyCrawler
     # lists all attrubutes of foody table
     def self.create_restaurant
         
-        Selenium::WebDriver::Chrome.path = "/app/bin/chromedriver"
+        Selenium::WebDriver::Chrome.path = "/app/.apt/usr/bin/google-chrome"
         Selenium::WebDriver::Chrome.driver_path = "/app/bin/chromedriver"
         
         @browser = Watir::Browser.new:chrome
